ac121be2fe38f2c6b40239063200a4bf5d551e33
aidaka <15143038938@163.com>
2020-05-29 11:48:56 +0800

    核心点
    进行操作笔记的总结
    和源码项目的导入

---
+++ sourcejava/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+[
++src/com/i2f/java/test/testhashmap/test.java,d\7\d7d4d21e7038035c641b77afff8e895a7e5894e2
+X
+(src/com/i2f/source/java/lang/Thread.java,d\9\d9bacf9b7d381eb58767557a5686bdaeb36cfafd
---
+++ sourcejava/src/com/i2f/java/test/testhashmap/test.java
@@ -0,0 +1,17 @@
+package com.i2f.java.test.testhashmap;
+
+import java.util.HashMap;
+
+/**
+ * @author LiuDaKa
+ * @create 2020-03-31 22:36
+ */
+public class test {
+    public static void main(String[] args) {
+        HashMap<String, Double> hashMap = new HashMap<>();
+        hashMap.put("k1",0.1);
+        System.out.println("liuyuchoa");
+    }
+
+    ;
+}
